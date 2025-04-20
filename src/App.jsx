bf669bef4dfca3cb9ab88806913fd70de3bb6f98
Guilherme Tavares <gui.tavares2002.contato@gmail.com>
2025-04-20 20:11:39 -0300

    Update files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,33 @@
+import Header from './components/Header.jsx'
+import UserInput from './components/UserInput.jsx'
+import './index.css'
+import {useState} from "react";
+
+
 function App() {
-  return (
-    <h1>React Investment Calculator</h1>
+
+    const [userInput,setUserInput] = useState({
+        initialInvestment: 10000,
+        annualInvestment: 1200,
+        expectedReturn: 6,
+        duration: 10
+    });
+
+    function handleChange(inputIdentifier, newValue) {
+        setUserInput(prevUserInput => {
+            return {
+                ...prevUserInput,
+                [inputIdentifier]: newValue,
+            }
+        });
+    }
+
+
+    return (
+      <>
+          <Header />
+          <UserInput onChange={handleChange}/>
+      </>
   )
 }
 
